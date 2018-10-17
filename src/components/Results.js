add8e5b1db31903d6e84825f95178b862397b543
Viren Bhagat <virenb@gmail.com>
2018-10-17 16:53:42 -0400

    added reactstrap for form styling and some inline styling done

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const Results = (props) => {
-  const { results } = props;
+  const { results } = props
+
   return (
     <div>
       <div>{results.map(animal => {
         return (
-          <div>
+          <div style={{ maxWidth: '480px', margin: '0 auto', borderBottom: '5px solid cornflowerblue' }}>
             <h3 key={animal.id.$t}>Meet {animal.name.$t}</h3>
-            <a href={animal.media.photos.photo[1].$t} target="_blank"><img src={animal.media.photos.photo[1].$t} alt="Picture of animal" /></a>
+            <a href={animal.media.photos.photo[3].$t} target="_blank" rel="noopener noreferrer"><img src={animal.media.photos.photo[3].$t} alt="animal" /></a>
             <p>Age: {animal.age.$t}</p>
-            <p>Breed: {animal.breeds.breed.$t}</p>
+            <p>Breed(s): {animal.breeds.breed.$t || animal.breeds.breed[0].$t + ', ' + animal.breeds.breed[1].$t}</p>
             <p>{animal.description.$t}</p>
             <p>{animal.contact.email.$t}</p>
           </div>
