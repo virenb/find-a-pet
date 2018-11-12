fa6f1fa40de311c206c4fb3d0c27f66352ce9682
Viren Bhagat <virenb@gmail.com>
2018-11-12 16:21:48 -0500

    fixed issue with pics in results

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,7 +9,10 @@ const Results = (props) => {
         return (
           <div key={animal.id.$t} style={{ maxWidth: '480px', margin: '0 auto', borderBottom: '5px solid cornflowerblue', borderRadius: '4px' }}>
             <h3>Meet {animal.name.$t}</h3>
+            {animal.media.photos === undefined ? <div></div> :
             <a href={animal.media.photos.photo[3].$t} target="_blank" rel="noopener noreferrer"><img src={animal.media.photos.photo[3].$t} alt="animal" /></a>
+            }
+            
             <p>Age: {animal.age.$t}</p>
             <p>Breed(s): {animal.breeds.breed.$t || animal.breeds.breed[0].$t + ', ' + animal.breeds.breed[1].$t}</p>
             <p>{animal.description.$t}</p>
